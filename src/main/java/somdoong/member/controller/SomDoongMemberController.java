4776fc6632c1f6ac4fd88b63e8dfe88d87d0fc9f
jonjalnam123 <peter2184@naver.com>
2022-11-24 15:25:40 +0900

    약관동의

--- src/main/java/somdoong/member/controller/SomDoongMemberController.java
+++ src/main/java/somdoong/member/controller/SomDoongMemberController.java
@@ -1,10 +1,21 @@
 package somdoong.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/member")
 public class SomDoongMemberController {
 
+	@RequestMapping("/agree")
+	public void agree() {
+		
+	}
 	
+	@GetMapping("/join")
+	public void join() {
+		
+	}
 	
 }
